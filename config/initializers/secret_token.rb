56f2ceed64929519b4006d6a4c3c2013bf3083ce
Dan Friel <ddfriel@gmail.com>
2013-09-09 16:51:06 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AptsCareers::Application.config.secret_token = '49b727b317267e0287a092670f19e70f1f2ec8ff01beeb7c989cb7b78487abc699eb09a7aeba63a793813c901ef44f5ca1ae0eff59f044632ef6054b8b960236'
